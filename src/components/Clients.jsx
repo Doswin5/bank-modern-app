1fd80b870211e65cd1230eea3f29f5dead55cb41
DOSWIN <ayomidedosunmuoreoluwa@gmail.com>
2022-12-13 23:27:44 +0100

    Second Commit

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { clients } from '../constants'
+
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <>
+      <div className="flex justify-between items-center px-[180px] my-[100px] w-full">
+        {
+          clients.map(client => (
+            <img src={client.logo} alt="client" className='h-[40px]' />
+          ))
+        }
+      </div>
+      
+      <div className="flex justify-between p-[80px] text-white bg-gray-800 rounded-3xl mx-[80px] items-center">
+        <div>
+          <h2>Let’s try our service now!</h2>
+          <p className='w-[55%] mt-4'>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        </div>
+        <button>Get Started</button>
+      </div>
+    </>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { feedback } from '../constants'
+import { quotes } from '../assets'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className="flex flex-col text-white px-[80px]">
+      <div className="flex justify-between items-center gap-x-40">
+        <h2>
+          What people are saying about us
+        </h2>
+        <p>
+          Everything you need to accept card payments and grow your business anywhere on the planet.
+        </p>
+      </div>
+
+      <div className="flex justify-between gap-16 mt-16">
+        {
+          feedback.map(item => (
+            <div className="flex flex-col justify-between w-full gap-4">
+              <img src={quotes} alt="quote" className='w-[40px]' />
+              <p>
+                {item.content}
+              </p>
+              <div className="flex gap-4 items-center">
+                <img src={item.img} alt="" />
+                <div className="flex flex-col gap-1">
+                  <h5>{item.name}</h5>
+                  <h6>{item.title}</h6>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
